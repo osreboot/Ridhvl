3bd34f1337a109cb54f27024244e1c2c99ca67cf
osreboot <calvin.16.weaver@gmail.com>
2015-04-01 18:42:35 -0700

    Added hvlPainter2D and two methods

---
+++ src/com/osreboot/ridhvl/painter/hvlPainter2D.java
@@ -0,0 +1,35 @@
+package com.osreboot.ridhvl.painter;
+
+import org.lwjgl.opengl.GL11;
+
+public class hvlPainter2D {
+
+	public static enum TemplateType{
+		DEFAULT
+	}
+
+	public static void GL11_Init(TemplateType type){
+		switch(type){
+		default:
+			GL11.glEnable(GL11.GL_TEXTURE_2D);
+			GL11.glEnable(GL11.GL_COLOR_MATERIAL);
+			GL11.glClearColor(0, 0, 0, 0);
+			GL11.glEnable(GL11.GL_BLEND);
+			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+			GL11.glMatrixMode(GL11.GL_MODELVIEW);
+			break;
+		}
+	}
+	
+	public static void GL11_Ortho(TemplateType type, int displayWidth, int displayHeight){
+		switch(type){
+		default:
+			GL11.glMatrixMode(GL11.GL_MATRIX_MODE);
+			GL11.glLoadIdentity();
+			GL11.glOrtho(0, displayWidth, displayHeight, 0, 1, -1);
+			GL11.glMatrixMode(GL11.GL_MODELVIEW);
+			break;
+		}
+	}
+
+}
