0162b7634e72580d5f7f4e0a9aae9153554b9c47
osreboot <calvin.16.weaver@gmail.com>
2015-04-13 15:43:51 -0700

    Added two font layouts

--- src/com/osreboot/ridhvl/HvlFontUtil.java
+++ src/com/osreboot/ridhvl/HvlFontUtil.java
@@ -3,17 +3,21 @@ package com.osreboot.ridhvl;
 public class HvlFontUtil {
 
 	private static char[] charsDefault = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '?', '.', '(', ')', '[', ']'};
+	private static char[] charsDefaultCase = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '?', '.', '(', ')', '[', ']'};
 	private static char[] charsNumerical = new char[]{'!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', ' '};
 	private static char[] charsSimplistic = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '!', '?', '"', '(', ')', '[', ']', ':', ';', '-', '+'};
+	private static char[] charsSimplisticCase = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '!', '?', '"', '(', ')', '[', ']', ':', ';', '-', '+'};
 
 	public static enum HvlFontLayout{
-		DEFAULT, NUMERICAL, SIMPLISTIC
+		DEFAULT, DEFAULTCASE, NUMERICAL, SIMPLISTIC, SIMPLISTICCASE
 	}
 
 	public static char[] getLayout(HvlFontLayout layout){
 		switch(layout){
 			case NUMERICAL: return charsNumerical;
 			case SIMPLISTIC: return charsSimplistic;
+			case SIMPLISTICCASE: return charsSimplisticCase;
+			case DEFAULTCASE: return charsDefaultCase;
 			default: return charsDefault;
 		}
 	}
