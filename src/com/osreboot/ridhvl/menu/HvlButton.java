650b614fbe1cce062e964529329e2f4f26216ec9
osreboot <calvin.16.weaver@gmail.com>
2015-05-30 12:10:48 -0700

    Fixed buttons again

--- src/com/osreboot/ridhvl/menu/HvlButton.java
+++ src/com/osreboot/ridhvl/menu/HvlButton.java
@@ -5,15 +5,15 @@ import java.util.ArrayList;
 import org.lwjgl.input.Mouse;
 
 public abstract class HvlButton {
-	
+
 	private static float globalInversionHeight;
-	
+
 	private static boolean triggered = false;
-	
+
 	public static void setInversionHeight(float inversionHeightArg){
 		globalInversionHeight = inversionHeightArg;
 	}
-	
+
 	public static float getInversionHeight(){
 		return globalInversionHeight;
 	}
@@ -21,11 +21,11 @@ public abstract class HvlButton {
 	public static void updateButtons(){
 		triggered = !Mouse.isButtonDown(0);
 	}
-	
+
 	private static ArrayList<HvlButton> buttons = new ArrayList<HvlButton>();
-	
+
 	private float xLocation, yLocation, xLength, yLength, inversionHeight;
-	
+
 	public HvlButton(float xArg, float yArg, float xlArg, float ylArg, float inversionHeightArg){
 		xLocation = xArg;
 		yLocation = yArg;
@@ -34,19 +34,19 @@ public abstract class HvlButton {
 		inversionHeight = inversionHeightArg;
 		buttons.add(this);
 	}
-	
+
 	public void onPressing(int buttonArg){}
 	public void onTriggered(){}
 	public void draw(long delta){}
-	
+
 	public final boolean isBeingPressed(int buttonArg){//TODO account for HvlDisplayMode
 		return Mouse.isInsideWindow() && Mouse.isButtonDown(buttonArg) && Mouse.getX() > xLocation && inversionHeight - Mouse.getY() > yLocation && Mouse.getX() < xLocation + xLength && inversionHeight - Mouse.getY() < yLocation + yLength;
 	}
-	
+
 	public final boolean isHovering(){//TODO account for HvlDisplayMode
 		return Mouse.isInsideWindow() && Mouse.getX() > xLocation && inversionHeight - Mouse.getY() > yLocation && Mouse.getX() < xLocation + xLength && inversionHeight - Mouse.getY() < yLocation + yLength;
 	}
-	
+
 	public final void update(long delta){
 		draw(delta);
 		if(isBeingPressed(0)){
@@ -55,25 +55,28 @@ public abstract class HvlButton {
 				onTriggered();
 				triggered = true;
 			}
-		}else if(isBeingPressed(1)){
-			onPressing(1);
+		}else{
+			if(isBeingPressed(1)){
+				onPressing(1);
+			}
+			triggered = false;
 		}
 	}
-	
+
 	public float getX(){
 		return xLocation;
 	}
-	
+
 	public float getY(){
 		return yLocation;
 	}
-	
+
 	public float getXLength(){
 		return xLength;
 	}
-	
+
 	public float getYLength(){
 		return yLength;
 	}
-	
+
 }
