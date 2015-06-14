5abd7016bbaf53ae294c6cfe6bfab3216486e844
osreboot <calvin.16.weaver@gmail.com>
2015-06-13 17:21:33 -0700

    Template2DBasicTest and TempalteInteg2DBasicTest now use a float delta and have correct font size values.

--- src/com/osreboot/ridhvl/test/Template2DBasicTest.java
+++ src/com/osreboot/ridhvl/test/Template2DBasicTest.java
@@ -13,37 +13,37 @@ import com.osreboot.ridhvl.painter.painter2d.HvlFontPainter2D;
 import com.osreboot.ridhvl.template.HvlTemplate2DBasic;
 
 public class Template2DBasicTest extends HvlTemplate2DBasic{
-	
+
 	public Template2DBasicTest(){
 		super(60, 1280, 720, "Unnamed", new HvlDisplayModeDefault());
 	}
-	
+
 	static HvlTextureLoader textureLoader = new HvlTextureLoader(5);
 	static HvlFontPainter2D fontPainter;
-	long gradient = 0;
-	
+	float gradient = 0;
+
 	@Override
 	public void initialize(){		
 		textureLoader.loadResource("White");
 		textureLoader.loadResource("Font");
-		
-		fontPainter = new HvlFontPainter2D(textureLoader.getResource(1), HvlFontUtil.DEFAULT, 2048, 2048, 40, 50, 51);
+
+		fontPainter = new HvlFontPainter2D(textureLoader.getResource(1), HvlFontUtil.DEFAULT, 2048, 2048, 112, 144, 18);
 	}
 
 	@Override
 	public void update(float delta){
-		gradient = gradient < 1280 ? gradient + ((long)delta * 1000) : 0;
-		
+		gradient = gradient < 1280 ? gradient + (delta*1000) : 0;
+
 		for(int i = 0; i < 360; i++){
 			hvlDrawQuad(0, i*2, gradient - (i*2), 2, textureLoader.getResource(0), new Color(1f, ((float)i - 180)/180, (float)i/360));
 		}
-		
+
 		hvlRotate(300f, 300f, gradient/1280f*360f);
 		hvlDrawQuad(250, 250, 100, 100, textureLoader.getResource(0));
 		hvlResetRotation();
-		
-		fontPainter.hvlDrawWord("test of the most basic template!", 10, 10, new Color(gradient/1280f, gradient/1280f, gradient/1280f, 1f));
-		fontPainter.hvlDrawWord("and rotation! yay?", 10, 100, new Color(1 - (gradient/1280f), 1 - (gradient/1280f), 1 - (gradient/1280f), 1f));
+
+		fontPainter.hvlDrawWord("test of the most basic template!", 10, 10, 1260, 100, new Color(gradient/1280f, gradient/1280f, gradient/1280f));
+		fontPainter.hvlDrawWord("and rotation! yay?", 10, 100, 0.5f, new Color(1 - (gradient/1280f), 1 - (gradient/1280f), 1 - (gradient/1280f)));
 	}
-	
+
 }
--- src/com/osreboot/ridhvl/test/TemplateInteg2DBasicTest.java
+++ src/com/osreboot/ridhvl/test/TemplateInteg2DBasicTest.java
@@ -18,19 +18,19 @@ public class TemplateInteg2DBasicTest extends HvlTemplateInteg2DBasic{
 	}
 
 	static HvlFontPainter2D fontPainter;
-	long gradient = 0;
+	float gradient = 0;
 	
 	@Override
 	public void initialize(){
 		getTextureLoader().loadResource("White");
 		getTextureLoader().loadResource("Font");
 		
-		fontPainter = new HvlFontPainter2D(getTextureLoader().getResource(1), HvlFontUtil.SIMPLISTIC, 2048, 2048, 112, 144, 18);
+		fontPainter = new HvlFontPainter2D(getTextureLoader().getResource(1), HvlFontUtil.DEFAULT, 2048, 2048, 112, 144, 18);
 	}
 
 	@Override
-	public void update(float delta){		
-		gradient = gradient < 1280 ? gradient + ((long)delta*1000) : 0;
+	public void update(float delta){
+		gradient = gradient < 1280 ? gradient + (delta*1000) : 0;
 		
 		for(int i = 0; i < 360; i++){
 			hvlDrawQuad(0, i*2, gradient - (i*2), 2, getTextureLoader().getResource(0), new Color(1f, ((float)i - 180)/180, (float)i/360));
@@ -40,8 +40,8 @@ public class TemplateInteg2DBasicTest extends HvlTemplateInteg2DBasic{
 		hvlDrawQuad(250, 250, 100, 100, getTextureLoader().getResource(0));
 		hvlResetRotation();
 		
-		fontPainter.hvlDrawWord("test of the most basic template!", 10, 10, new Color(gradient/1280f, gradient/1280f, gradient/1280f, 1f));
-		fontPainter.hvlDrawWord("and rotation! yay?", 10, 100, new Color(1 - (gradient/1280f), 1 - (gradient/1280f), 1 - (gradient/1280f), 1f));
+		fontPainter.hvlDrawWord("test of the most basic template!", 10, 10, 1260, 100, new Color(gradient/1280f, gradient/1280f, gradient/1280f));
+		fontPainter.hvlDrawWord("and rotation! yay?", 10, 100, 0.5f, new Color(1 - (gradient/1280f), 1 - (gradient/1280f), 1 - (gradient/1280f)));
 
 	}
 
