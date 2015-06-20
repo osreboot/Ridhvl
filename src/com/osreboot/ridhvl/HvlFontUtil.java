187c06d5ffc34edf9884495c8c8d281e4e42358c
Adam Krpan <creeperinatardis@gmail.com>
2015-06-20 13:10:47 -0700

    Added a math font that's just numbers and symbols.

--- src/com/osreboot/ridhvl/HvlFontUtil.java
+++ src/com/osreboot/ridhvl/HvlFontUtil.java
@@ -8,6 +8,7 @@ public class HvlFontUtil {
 	public static final char[] NUMERICAL = new char[]{'!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', ' '};
 	public static final char[] SIMPLISTIC = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '!', '?', '"', '(', ')', '[', ']', ':', ';', '-', '+'};
 	public static final char[] SIMPLISTICCASE = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '!', '?', '"', '(', ')', '[', ']', ':', ';', '-', '+'};
+	public static final char[] MATHEMATICS = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/' };
 	
 	public static boolean containsChar(char[] layout, char c){
 		for(int i = 0; i < layout.length; i++){
