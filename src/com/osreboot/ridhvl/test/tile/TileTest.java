15b23ce3e1b8930f2b8d9d67d5a03df7aa57787c
Adam Krpan <creeperinatardis@gmail.com>
2015-06-20 09:08:37 -0700

    Changed some stuff, I don't know what.

--- src/com/osreboot/ridhvl/test/tile/TileTest.java
+++ src/com/osreboot/ridhvl/test/tile/TileTest.java
@@ -9,31 +9,49 @@ import com.osreboot.ridhvl.loader.HvlTextureLoader;
 import com.osreboot.ridhvl.template.HvlTemplate2DBasic;
 import com.osreboot.ridhvl.tile.HvlTileMap;
 
-public class TileTest extends HvlTemplate2DBasic{
+public class TileTest extends HvlTemplate2DBasic {
 
-	public TileTest(){
+	public TileTest() {
 		super(60, 1280, 720, "Unnamed", new HvlDisplayModeDefault());
 	}
 
 	static HvlTextureLoader textureLoader = new HvlTextureLoader(5);
-	
+
 	private HvlTileMap tilemap;
-	
+
+	public static void main(String[] args) {
+		new TileTest().start();
+	}
+
 	@Override
-	public void initialize(){		
+	public void initialize() {
 		textureLoader.loadResource("White");
-		textureLoader.loadResource("TestTilesheet");
-		
+		textureLoader.loadResource("TestTilemap");
+
+//		tilemap = new HvlTileMap(textureLoader.getResource(1), 16, 16, 16, 16, 0,
+//				0, 64, 64);
+//		tilemap.fill(new HvlSimpleTile(0));
+//		tilemap.setTile(0, 0, new HvlSimpleTile(96));
+//		try {
+//			BufferedWriter writer = new BufferedWriter(new FileWriter(
+//					"res/SavedMap.map"));
+//			writer.write(HvlTileMap.save(tilemap));
+//			writer.close();
+//		} catch (IOException e1) {
+//			
+//		}
+
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader("res/TestLevelSave.txt"));
+			BufferedReader reader = new BufferedReader(new FileReader(
+					"res/SavedMap.map"));
 			StringBuilder sb = new StringBuilder();
 			String current;
-			while ((current = reader.readLine()) != null)
-			{
+			while ((current = reader.readLine()) != null) {
 				sb.append(current);
 				sb.append(System.lineSeparator());
 			}
-			tilemap = HvlTileMap.load(sb.toString(), textureLoader.getResource(1), 0, 0, 64, 64);
+			tilemap = HvlTileMap.load(sb.toString(),
+					textureLoader.getResource(1), 0, 0, 64, 64);
 			reader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -41,7 +59,7 @@ public class TileTest extends HvlTemplate2DBasic{
 	}
 
 	@Override
-	public void update(float delta){
+	public void update(float delta) {
 		tilemap.draw(delta);
 	}
 
