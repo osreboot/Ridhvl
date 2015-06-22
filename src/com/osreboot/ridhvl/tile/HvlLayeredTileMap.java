998329a585f7986a5492b194ba69bd1337965bc5
Adam Krpan <creeperinatardis@gmail.com>
2015-06-21 19:47:55 -0700

    Added getters and setters.

--- src/com/osreboot/ridhvl/tile/HvlLayeredTileMap.java
+++ src/com/osreboot/ridhvl/tile/HvlLayeredTileMap.java
@@ -57,4 +57,37 @@ public class HvlLayeredTileMap {
 		
 		return sb.toString();
 	}
+
+	
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getTileWidth() {
+		return tileWidth;
+	}
+
+	public void setTileWidth(float tileWidth) {
+		this.tileWidth = tileWidth;
+	}
+
+	public float getTileHeight() {
+		return tileHeight;
+	}
+
+	public void setTileHeight(float tileHeight) {
+		this.tileHeight = tileHeight;
+	}
 }
