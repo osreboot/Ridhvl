9b2b3a0ca4c40c94f2e9bd9221719ed195a3a1e4
osreboot <calvin.16.weaver@gmail.com>
2015-06-22 21:46:28 -0700

    Added the HvlPainter2D flag TEXMAGBLUR

--- src/com/osreboot/ridhvl/painter/painter2d/HvlPainter2D.java
+++ src/com/osreboot/ridhvl/painter/painter2d/HvlPainter2D.java
@@ -1,6 +1,6 @@
 package com.osreboot.ridhvl.painter.painter2d;
 
-import org.lwjgl.opengl.GL11;
+import static org.lwjgl.opengl.GL11.*;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.opengl.Texture;
 
@@ -13,22 +13,34 @@ public class HvlPainter2D {
 	public static void hvlGL11Init(HvlPainter2DProfile profile){
 		switch(profile){
 		default:
-			GL11.glEnable(GL11.GL_TEXTURE_2D);
-			GL11.glEnable(GL11.GL_COLOR_MATERIAL);
-			GL11.glClearColor(0, 0, 0, 0);
-			GL11.glEnable(GL11.GL_BLEND);
-			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-			GL11.glMatrixMode(GL11.GL_MODELVIEW);
+			glEnable(GL_TEXTURE_2D);
+			glEnable(GL_COLOR_MATERIAL);
+			glClearColor(0, 0, 0, 0);
+			glEnable(GL_BLEND);
+			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+			glMatrixMode(GL_MODELVIEW);
 			break;
 		}
 	}
 
 	public static void hvlGL11Ortho(int displayWidth, int displayHeight){
-		GL11.glMatrixMode(GL11.GL_MATRIX_MODE);
-		GL11.glLoadIdentity();
-		GL11.glOrtho(0, displayWidth, displayHeight, 0, 1, -1);
-		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		glMatrixMode(GL_MATRIX_MODE);
+		glLoadIdentity();
+		glOrtho(0, displayWidth, displayHeight, 0, 1, -1);
+		glMatrixMode(GL_MODELVIEW);
 	}
+	
+	public static HvlFlag2D TEXMAGBLUR = new HvlFlag2D(true){
+		@Override
+		public void enable(){
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		}
+		
+		@Override
+		public void disable(){
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		}
+	};
 	
 	//TODO account for HvlDisplayMode
 	public static void hvlDrawQuad(float x, float y, float xl, float yl, Texture t){
