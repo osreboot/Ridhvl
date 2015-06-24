ec042689d68139085d18632a955d8f3f414c73a4
osreboot <calvin.16.weaver@gmail.com>
2015-06-23 21:20:53 -0700

    Organized some imports

--- src/com/osreboot/ridhvl/painter/painter2d/HvlQuadPainter2D.java
+++ src/com/osreboot/ridhvl/painter/painter2d/HvlQuadPainter2D.java
@@ -1,85 +1,86 @@
 package com.osreboot.ridhvl.painter.painter2d;
 
 
-import org.lwjgl.opengl.GL11;
+import static org.lwjgl.opengl.GL11.*;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.opengl.Texture;
 
 class HvlQuadPainter2D {
 	
 	protected static void hvlDrawQuad(float x, float y, float xl, float yl, Texture t){
-		GL11.glColor4f(1, 1, 1, 1);
+		glColor4f(1, 1, 1, 1);
 		t.bind();
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glTexCoord2f(0, 0);
-		GL11.glVertex2f(x, y);
-		GL11.glTexCoord2f(1, 0);
-		GL11.glVertex2f(x + xl, y);
-		GL11.glTexCoord2f(1, 1);
-		GL11.glVertex2f(x + xl, y + yl);
-		GL11.glTexCoord2f(0, 1);
-		GL11.glVertex2f(x, y + yl);
-		GL11.glEnd();
+		glBegin(GL_QUADS);
+		glTexCoord2f(0, 0);
+		glVertex2f(x, y);
+		glTexCoord2f(1, 0);
+		glVertex2f(x + xl, y);
+		glTexCoord2f(1, 1);
+		glVertex2f(x + xl, y + yl);
+		glTexCoord2f(0, 1);
+		glVertex2f(x, y + yl);
+		glEnd();
 	}
 	
 	protected static void hvlDrawQuad(float x, float y, float xl, float yl, Texture t, Color c){
-		GL11.glColor4f(c.r, c.g, c.b, c.a);
+		glColor4f(c.r, c.g, c.b, c.a);
 		t.bind();
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glTexCoord2f(0, 0);
-		GL11.glVertex2f(x, y);
-		GL11.glTexCoord2f(1, 0);
-		GL11.glVertex2f(x + xl, y);
-		GL11.glTexCoord2f(1, 1);
-		GL11.glVertex2f(x + xl, y + yl);
-		GL11.glTexCoord2f(0, 1);
-		GL11.glVertex2f(x, y + yl);
-		GL11.glEnd();
+		glBegin(GL_QUADS);
+		glTexCoord2f(0, 0);
+		glVertex2f(x, y);
+		glTexCoord2f(1, 0);
+		glVertex2f(x + xl, y);
+		glTexCoord2f(1, 1);
+		glVertex2f(x + xl, y + yl);
+		glTexCoord2f(0, 1);
+		glVertex2f(x, y + yl);
+		glEnd();
 	}
 	
 	protected static void hvlDrawQuad(float x, float y, float xl, float yl, float uvx1, float uvy1, float uvx2, float uvy2, Texture t){
-		GL11.glColor4f(1, 1, 1, 1);
+		glColor4f(1, 1, 1, 1);
 		t.bind();
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glTexCoord2f(uvx1, uvy1);
-		GL11.glVertex2f(x, y);
-		GL11.glTexCoord2f(uvx2, uvy1);
-		GL11.glVertex2f(x + xl, y);
-		GL11.glTexCoord2f(uvx2, uvy2);
-		GL11.glVertex2f(x + xl, y + yl);
-		GL11.glTexCoord2f(uvx1, uvy2);
-		GL11.glVertex2f(x, y + yl);
-		GL11.glEnd();
+		glBegin(GL_QUADS);
+		glTexCoord2f(uvx1, uvy1);
+		glVertex2f(x, y);
+		glTexCoord2f(uvx2, uvy1);
+		glVertex2f(x + xl, y);
+		glTexCoord2f(uvx2, uvy2);
+		glVertex2f(x + xl, y + yl);
+		glTexCoord2f(uvx1, uvy2);
+		glVertex2f(x, y + yl);
+		glEnd();
 	}
 	
 	protected static void hvlDrawQuad(float x, float y, float xl, float yl, float uvx1, float uvy1, float uvx2, float uvy2, Texture t, Color c){
-		GL11.glColor4f(c.r, c.g, c.b, c.a);
+		glColor4f(c.r, c.g, c.b, c.a);
 		t.bind();
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glTexCoord2f(uvx1, uvy1);
-		GL11.glVertex2f(x, y);
-		GL11.glTexCoord2f(uvx2, uvy1);
-		GL11.glVertex2f(x + xl, y);
-		GL11.glTexCoord2f(uvx2, uvy2);
-		GL11.glVertex2f(x + xl, y + yl);
-		GL11.glTexCoord2f(uvx1, uvy2);
-		GL11.glVertex2f(x, y + yl);
-		GL11.glEnd();
+		glBegin(GL_QUADS);
+		glTexCoord2f(uvx1, uvy1);
+		glVertex2f(x, y);
+		glTexCoord2f(uvx2, uvy1);
+		glVertex2f(x + xl, y);
+		glTexCoord2f(uvx2, uvy2);
+		glVertex2f(x + xl, y + yl);
+		glTexCoord2f(uvx1, uvy2);
+		glVertex2f(x, y + yl);
+		glEnd();
 	}
 	
 	protected static void hvlDrawQuad(float x, float y, float xl, float yl, int textureID){
-		GL11.glColor4f(1, 1, 1, 1);
-		GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureID);
-		GL11.glBegin(GL11.GL_QUADS);
-		GL11.glTexCoord2f(0, 0);
-		GL11.glVertex2f(x, y);
-		GL11.glTexCoord2f(1, 0);
-		GL11.glVertex2f(x + xl, y);
-		GL11.glTexCoord2f(1, 1);
-		GL11.glVertex2f(x + xl, y + yl);
-		GL11.glTexCoord2f(0, 1);
-		GL11.glVertex2f(x, y + yl);
-		GL11.glEnd();
+		glColor4f(1, 1, 1, 1);
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glBegin(GL_QUADS);
+		glTexCoord2f(0, 0);
+		glVertex2f(x, y);
+		glTexCoord2f(1, 0);
+		glVertex2f(x + xl, y);
+		glTexCoord2f(1, 1);
+		glVertex2f(x + xl, y + yl);
+		glTexCoord2f(0, 1);
+		glVertex2f(x, y + yl);
+		glEnd();
 	}
 	
 	//TODO hvlDrawQuad(float x, float y, float xl, float yl, Color c)
