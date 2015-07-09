50623c1f14f509b03e2b4ed7464d4f10cdb2e467
Adam Krpan <creeperinatardis@gmail.com>
2015-07-08 17:11:39 -0700

    Removed the entity system (will be recoding later to fit our vision better).

--- src/com/osreboot/ridhvl/entity/HvlEntity.java
+++
@@ -1,9 +0,0 @@
-package com.osreboot.ridhvl.entity;
-
-public abstract class HvlEntity {
-	public abstract void initialize();
-	
-	public abstract void update(float delta);
-	
-	public abstract void draw(float delta);
-}
