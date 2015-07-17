9922fc989bfe78e72741c62deeeb591e442d09f6
osreboot <calvin.16.weaver@gmail.com>
2015-07-17 10:52:34 -0700

    Finished HvlTextureSeriesLoader

--- src/com/osreboot/ridhvl/loader/HvlTextureSeriesLoader.java
+++ src/com/osreboot/ridhvl/loader/HvlTextureSeriesLoader.java
@@ -11,20 +11,21 @@ public class HvlTextureSeriesLoader extends HvlContentSeriesLoader<Texture>{
 	public HvlTextureSeriesLoader(String defaultPathArg, int arrayLength){
 		super(Texture.class, defaultPathArg, arrayLength);
 	}
-	
+
 	public HvlTextureSeriesLoader(int arrayLength){
 		super(Texture.class, "res/", arrayLength);
 	}
 
 	@Override
 	public boolean loadResource(String nameArg, int lengthArg) {
-		ArrayList<Texture> resources = new ArrayList<Texture>();//TODO
-		Texture t;
-		try{
-			t = TextureLoader.getTexture("PNG", new FileInputStream(getPath() + nameArg + ".png"));
-		}catch(Exception e){
-			e.printStackTrace();
-			return false;
+		ArrayList<Texture> resources = new ArrayList<Texture>();
+		for(int i = 0; i < lengthArg; i++){
+			try{
+				resources.add(TextureLoader.getTexture("PNG", new FileInputStream(getPath() + String.format(nameArg, lengthArg) + ".png")));
+			}catch(Exception e){
+				e.printStackTrace();
+				return false;
+			}
 		}
 		super.addResource(resources);
 		return true;
