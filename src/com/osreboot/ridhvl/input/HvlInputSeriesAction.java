7919e1ef83429429b6eb9a387c7180922651aeb9
osreboot <calvin.16.weaver@gmail.com>
2015-08-09 21:33:50 -0700

    Fixed a misplaced negative

--- src/com/osreboot/ridhvl/input/HvlInputSeriesAction.java
+++ src/com/osreboot/ridhvl/input/HvlInputSeriesAction.java
@@ -23,7 +23,7 @@ public class HvlInputSeriesAction {
 	
 	public static final HvlInputFilter FILTER_DOWN = new HvlInputFilter(){
 		public float getCurrentOutput(){
-			return Keyboard.isKeyDown(Keyboard.KEY_S) || Keyboard.isKeyDown(Keyboard.KEY_DOWN) ? -1 : 0;
+			return Keyboard.isKeyDown(Keyboard.KEY_S) || Keyboard.isKeyDown(Keyboard.KEY_DOWN) ? 1 : 0;
 		}
 	};
 
