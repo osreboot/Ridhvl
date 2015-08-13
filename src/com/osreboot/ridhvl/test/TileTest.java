354872bab92dd14e5ee7cecab623806766a8f458
Adam Krpan <creeperinatardis@gmail.com>
2015-08-13 10:41:04 -0700

    Began enforcing a common tilemap extension and location (res folder and .hvlmap) and supported inline text loading for a map (in the load method). Close #110 and close #103

--- src/com/osreboot/ridhvl/test/TileTest.java
+++ src/com/osreboot/ridhvl/test/TileTest.java
@@ -1,16 +1,12 @@
 package com.osreboot.ridhvl.test;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
 import com.osreboot.ridhvl.loader.HvlTextureLoader;
 import com.osreboot.ridhvl.template.HvlTemplate2D;
 import com.osreboot.ridhvl.tile.HvlLayeredTileMap;
 
 public class TileTest extends HvlTemplate2D {
-	
+
 	public TileTest() {
 		super(60, 1280, 720, "Ridhvl TileMap Test", new HvlDisplayModeDefault());
 	}
@@ -18,52 +14,40 @@ public class TileTest extends HvlTemplate2D {
 	static HvlTextureLoader textureLoader = new HvlTextureLoader();
 
 	private HvlLayeredTileMap tilemaps;
-	
+
 	@Override
 	public void initialize() {
 		textureLoader.loadResource("Icon");
 
-//		tilemaps = new HvlTileMap[] {new HvlTileMap(textureLoader.getResource(0), 16, 16, 16, 16, 0,
-//				0, 64, 64), new HvlTileMap(textureLoader.getResource(0), 16, 16, 16, 16, 0,
-//						0, 64, 64)};
-//		tilemaps[0].fill(new HvlSimpleTile(0));
-//		tilemaps[0].setTile(0, 0, new HvlSimpleTile(96));
-//		tilemaps[1].setTile(1, 0, new HvlSimpleTile(1));
-//		tilemaps[1].setTile(2, 0, new HvlAnimatedTile(1.0f, 1, 2, 18));
-//		try {
-//			BufferedWriter writer = new BufferedWriter(new FileWriter(
-//					"res/SavedMap.map"));
-//			writer.write(HvlTileMap.save(tilemaps));
-//			writer.close();
-//		} catch (IOException e1) {
-//			
-//		}
+		// tilemaps = new HvlTileMap[] {new
+		// HvlTileMap(textureLoader.getResource(0), 16, 16, 16, 16, 0,
+		// 0, 64, 64), new HvlTileMap(textureLoader.getResource(0), 16, 16, 16,
+		// 16, 0,
+		// 0, 64, 64)};
+		// tilemaps[0].fill(new HvlSimpleTile(0));
+		// tilemaps[0].setTile(0, 0, new HvlSimpleTile(96));
+		// tilemaps[1].setTile(1, 0, new HvlSimpleTile(1));
+		// tilemaps[1].setTile(2, 0, new HvlAnimatedTile(1.0f, 1, 2, 18));
+		// try {
+		// BufferedWriter writer = new BufferedWriter(new FileWriter(
+		// "res/SavedMap.map"));
+		// writer.write(HvlTileMap.save(tilemaps));
+		// writer.close();
+		// } catch (IOException e1) {
+		//
+		// }
 
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(
-					"res/SavedMap.map"));
-			StringBuilder sb = new StringBuilder();
-			String current;
-			while ((current = reader.readLine()) != null) {
-				sb.append(current);
-				sb.append(System.lineSeparator());
-			}
-			tilemaps = HvlLayeredTileMap.load(sb.toString(),  textureLoader.getResource(0),  0, 0, 64, 64);
-			tilemaps.setCutOff(true);
-			tilemaps.setxLeft(0);
-			tilemaps.setxRight(512);
-			tilemaps.setyTop(0);
-			tilemaps.setyBottom(256);
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		tilemaps = HvlLayeredTileMap.load("SavedMap", true, textureLoader.getResource(0), 0, 0, 64, 64);
+		tilemaps.setCutOff(true);
+		tilemaps.setxLeft(0);
+		tilemaps.setxRight(512);
+		tilemaps.setyTop(0);
+		tilemaps.setyBottom(256);
 	}
 
 	@Override
 	public void update(float delta) {
-		if (tilemaps != null)
-		{			
+		if (tilemaps != null) {
 			tilemaps.draw(delta);
 		}
 	}
