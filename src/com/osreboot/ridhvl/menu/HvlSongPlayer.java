f5bfb70d0d7d95ae23dba2854bee0b6c39202b68
osreboot <calvin.16.weaver@gmail.com>
2015-08-19 00:05:35 -0700

    Fixed HvlSongPlayer and added song inheritance

--- src/com/osreboot/ridhvl/menu/HvlSongPlayer.java
+++ src/com/osreboot/ridhvl/menu/HvlSongPlayer.java
@@ -9,28 +9,52 @@ import com.osreboot.ridhvl.HvlMath;
 
 public class HvlSongPlayer {
 
-	private static HashMap<Audio, HvlMenu> songs = new HashMap<Audio, HvlMenu>();
+	private static HashMap<HvlMenu, ArrayList<Audio>> songs = new HashMap<>();
+	private static HashMap<HvlMenu, ArrayList<HvlMenu>> children = new HashMap<>();
 
 	private static float fadeTime, currentFade = 0;//TODO integrate this
 	private static Audio currentSong;
 
-	public static void update(float delta){
+	public static void update(float delta){//TODO exception for nonexistent parent
 		if(songs.size() > 0){
-			if(currentSong == null || songs.get(currentSong) != HvlMenu.getCurrent() || !currentSong.isPlaying()){
-				if(songs.entrySet().contains(HvlMenu.getCurrent())){
-					ArrayList<Audio> newSongOptions = new ArrayList<Audio>();
-					for(Audio a : songs.keySet()) if(songs.get(a) == HvlMenu.getCurrent()) newSongOptions.add(a);
+			if(currentSong != null && !currentSong.isPlaying()) currentSong = null;
+
+			HvlMenu ultimate = null;
+			if(songs.keySet().contains(HvlMenu.getCurrent())) ultimate = HvlMenu.getCurrent(); else{
+				for(HvlMenu menu : children.keySet()) if(children.get(menu).contains(HvlMenu.getCurrent())) ultimate = menu;
+			}
+
+			if(ultimate != null){
+				if(currentSong == null || !songs.get(ultimate).contains(currentSong)){
+					ArrayList<Audio> newSongOptions = new ArrayList<>();
+					for(Audio a : songs.get(ultimate)) newSongOptions.add(a);
+
+					if(currentSong != null) currentSong.stop();
+						
 					if(currentSong != null) if(newSongOptions.contains(currentSong) && newSongOptions.size() > 1) newSongOptions.remove(currentSong);
+
 					currentSong = newSongOptions.get(HvlMath.getRand().nextInt(newSongOptions.size()));
 					currentSong.playAsSoundEffect(1, 1, false);
 				}
+			}else{
+				if(currentSong != null){
+					currentSong.stop();
+					currentSong = null;
+				}
 			}
-
 		}
 	}
 
-	public static void addSong(Audio audio, HvlMenu menu){
-		songs.put(audio, menu);
+	public static void addSong(HvlMenu menu, Audio audio){
+		if(!songs.containsKey(menu)) songs.put(menu, new ArrayList<Audio>());
+		songs.get(menu).add(audio);
+	}
+
+	public static void addMenuChild(HvlMenu parent, HvlMenu child){
+		if(!songs.containsKey(child)){
+			if(!children.containsKey(parent)) children.put(parent, new ArrayList<HvlMenu>());
+			children.get(parent).add(child);
+		}else System.err.println("Child menu already has bound songs!");//TODO throw exception
 	}
 
 	public static float getFadeTime(){
