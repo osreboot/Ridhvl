982fde9a3083930defa38e157d1771edfde05685
osreboot <calvin.16.weaver@gmail.com>
2015-08-19 01:21:13 -0700

    Added song fades and close #109

--- src/com/osreboot/ridhvl/menu/HvlSongPlayer.java
+++ src/com/osreboot/ridhvl/menu/HvlSongPlayer.java
@@ -10,39 +10,60 @@ import com.osreboot.ridhvl.HvlMath;
 public class HvlSongPlayer {
 
 	private static HashMap<HvlMenu, ArrayList<Audio>> songs = new HashMap<>();
-	private static HashMap<HvlMenu, ArrayList<HvlMenu>> children = new HashMap<>();
 
-	private static float fadeTime, currentFade = 0;//TODO integrate this
+	private static float fadeTime = 0, fadeGoal = 0, fadeLocation = 0, volume = 1f;//TODO integrate this
 	private static Audio currentSong;
 
-	public static void update(float delta){//TODO exception for nonexistent parent
-		if(songs.size() > 0){
-			if(currentSong != null && !currentSong.isPlaying()) currentSong = null;
+	public static void update(float delta){
+		if(songs.size() > 0){//if this system is being used
 
-			HvlMenu ultimate = null;
-			if(songs.keySet().contains(HvlMenu.getCurrent())) ultimate = HvlMenu.getCurrent(); else{
-				for(HvlMenu menu : children.keySet()) if(children.get(menu).contains(HvlMenu.getCurrent())) ultimate = menu;
-			}
-
-			if(ultimate != null){
-				if(currentSong == null || !songs.get(ultimate).contains(currentSong)){
-					ArrayList<Audio> newSongOptions = new ArrayList<>();
-					for(Audio a : songs.get(ultimate)) newSongOptions.add(a);
-
-					if(currentSong != null) currentSong.stop();
-						
-					if(currentSong != null) if(newSongOptions.contains(currentSong) && newSongOptions.size() > 1) newSongOptions.remove(currentSong);
-
-					currentSong = newSongOptions.get(HvlMath.getRand().nextInt(newSongOptions.size()));
-					currentSong.playAsSoundEffect(1, 1, false);
-				}
-			}else{
-				if(currentSong != null){
+			if(fadeTime > 0){
+				float previousLocation = fadeLocation;//TODO changing volume like this makes a strange clicking noise
+				fadeLocation = HvlMath.stepTowards(fadeLocation, delta * (1/fadeTime), fadeGoal * volume);
+				if(currentSong != null && previousLocation != fadeLocation){
+					float position = currentSong.getPosition();
 					currentSong.stop();
-					currentSong = null;
+					currentSong.playAsSoundEffect(1, fadeLocation, false);
+					currentSong.setPosition(position);
 				}
 			}
+
+			if(songs.containsKey(HvlMenu.getCurrent())){//if the menu has playable songs
+				if(!songs.get(HvlMenu.getCurrent()).contains(currentSong)) endCurrentSong();//if the current song is not in menu's list
+
+				if(currentSong == null || !currentSong.isPlaying()){//if nothing is playing
+					playRandomSong(songs.get(HvlMenu.getCurrent()));//play a tune
+				}
+			}else endCurrentSong();//end the song if the menu does not have any songs
 		}
+	}
+
+	private static void endCurrentSong(){
+		if(currentSong != null){
+			currentSong.stop();
+			currentSong = null;
+
+			fadeGoal = 0;
+			fadeLocation = 0;
+		}
+	}
+
+	private static void playRandomSong(ArrayList<Audio> songArgs){
+		ArrayList<Audio> newSongOptions = new ArrayList<>();
+		for(Audio a : songArgs) newSongOptions.add(a);
+
+		if(currentSong != null) if(newSongOptions.contains(currentSong) && newSongOptions.size() > 1) newSongOptions.remove(currentSong);
+
+		currentSong = newSongOptions.get(HvlMath.getRand().nextInt(newSongOptions.size()));
+		currentSong.playAsSoundEffect(1, fadeTime > 0 ? 0 : 1, false);
+
+		fadeGoal = 1;
+		fadeLocation = 0;
+	}
+
+	public static void fadeOut(){
+		fadeGoal = 0;
+		fadeLocation = 1;
 	}
 
 	public static void addSong(HvlMenu menu, Audio audio){
@@ -50,11 +71,10 @@ public class HvlSongPlayer {
 		songs.get(menu).add(audio);
 	}
 
-	public static void addMenuChild(HvlMenu parent, HvlMenu child){
-		if(!songs.containsKey(child)){
-			if(!children.containsKey(parent)) children.put(parent, new ArrayList<HvlMenu>());
-			children.get(parent).add(child);
-		}else System.err.println("Child menu already has bound songs!");//TODO throw exception
+	public static void addAllSongs(HvlMenu parent, HvlMenu child){
+		if(songs.containsKey(parent)){
+			songs.put(child, songs.get(parent));
+		}else System.err.print("Parent menu doesn't have any songs!");//TODO else throw exception
 	}
 
 	public static float getFadeTime(){
@@ -67,6 +87,14 @@ public class HvlSongPlayer {
 
 	public static Audio getCurrentSong(){
 		return currentSong;
+	}
+
+	public static float getVolume(){
+		return volume;
+	}
+
+	public static void setVolume(float volumeArg){
+		volume = volumeArg;
 	}
 
 }
