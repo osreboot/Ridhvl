4321ef31c274fad4c75c9cdca34d6e74577cdb9f
Adam Krpan <creeperinatardis@gmail.com>
2015-08-19 19:56:48 -0700

    Began working on a new system of cloning HvlComponents (#74). Not used for anything yet, but it's there.

---
+++ src/com/osreboot/ridhvl/menu/reflect/DoNotClone.java
@@ -0,0 +1,9 @@
+package com.osreboot.ridhvl.menu.reflect;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DoNotClone {
+
+}
