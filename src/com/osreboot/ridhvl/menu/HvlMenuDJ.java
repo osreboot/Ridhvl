3346065abde19457e58edc0410703e42865e6189
osreboot <calvin.16.weaver@gmail.com>
2015-08-24 18:01:29 -0700

    Hackish fix for song volume. (Please revise)

--- src/com/osreboot/ridhvl/menu/HvlMenuDJ.java
+++ src/com/osreboot/ridhvl/menu/HvlMenuDJ.java
@@ -14,16 +14,25 @@ public class HvlMenuDJ {
 	private static float fadeTime = 0, fadeGoal = 0, fadeLocation = 0, volume = 1f;//TODO integrate this
 	private static Audio currentSong;
 
+	private static float lastVolume = 0;
+	
 	public static void update(float delta){
 		if(songs.size() > 0){//if this system is being used
 
+			if(currentSong != null && lastVolume != volume){
+				float position = currentSong.getPosition();
+				currentSong.stop();
+				currentSong.playAsSoundEffect(1, volume, false);
+				currentSong.setPosition(position);
+			}
+				
 			if(fadeTime > 0 && volume > 0){
 				float previousLocation = fadeLocation;//TODO changing volume like this makes a strange clicking noise
 				fadeLocation = HvlMath.stepTowards(fadeLocation, delta * (1/fadeTime) * volume, fadeGoal * volume);
 				if(currentSong != null && previousLocation != fadeLocation){
 					float position = currentSong.getPosition();
 					currentSong.stop();
-					currentSong.playAsSoundEffect(1, fadeLocation, false);
+					currentSong.playAsSoundEffect(1, fadeLocation * volume, false);
 					currentSong.setPosition(position);
 				}
 			}
@@ -31,10 +40,12 @@ public class HvlMenuDJ {
 			if(songs.containsKey(HvlMenu.getCurrent())){//if the menu has playable songs
 				if(!songs.get(HvlMenu.getCurrent()).contains(currentSong)) endCurrentSong();//if the current song is not in menu's list
 
-				if(currentSong == null || !currentSong.isPlaying()){//if nothing is playing
+				if(currentSong == null || !currentSong.isPlaying() && volume > 0){//if nothing is playing
 					playRandomSong(songs.get(HvlMenu.getCurrent()));//play a tune
 				}
 			}else endCurrentSong();//end the song if the menu does not have any songs
+			
+			lastVolume = volume;
 		}
 	}
 
