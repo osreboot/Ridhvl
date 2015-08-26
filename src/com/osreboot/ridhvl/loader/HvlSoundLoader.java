29b895a81e94a7fa7618d1de933341ff12f85037
osreboot <calvin.16.weaver@gmail.com>
2015-08-25 22:43:33 -0700

    Added [untested] .mp3 loading support

--- src/com/osreboot/ridhvl/loader/HvlSoundLoader.java
+++ src/com/osreboot/ridhvl/loader/HvlSoundLoader.java
@@ -8,10 +8,14 @@ import org.newdawn.slick.openal.AudioLoader;
 
 public class HvlSoundLoader extends HvlContentLoader<Audio>{
 
+	public enum HvlSoundType{
+		WAV, MP3
+	}
+
 	public HvlSoundLoader(String defaultPathArg){
 		super(defaultPathArg);
 	}
-	
+
 	public HvlSoundLoader(){
 		super("res/");
 	}
@@ -21,6 +25,21 @@ public class HvlSoundLoader extends HvlContentLoader<Audio>{
 		Audio t;
 		try{
 			t = AudioLoader.getAudio("WAV", new BufferedInputStream(new FileInputStream(getPath() + nameArg + ".wav")));
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		super.addResource(t);
+		return true;
+	}
+
+	public boolean loadResource(String nameArg, HvlSoundType typeArg){
+		Audio t;
+		try{
+			switch(typeArg){
+			case MP3: t = AudioLoader.getAudio("MP3", new BufferedInputStream(new FileInputStream(getPath() + nameArg + ".mp3"))); break; //TODO test this
+			default: t = AudioLoader.getAudio("WAV", new BufferedInputStream(new FileInputStream(getPath() + nameArg + ".wav"))); break;
+			}
 		}catch(Exception e){
 			e.printStackTrace();
 			return false;
