0f21ebcc9acbc14aaa865dd9d9ff09ea567fdcf0
osreboot <calvin.16.weaver@gmail.com>
2015-09-19 18:54:46 -0700

    HvlSequentialLoader tweaks

--- src/com/osreboot/ridhvl/loader/HvlSequentialLoader.java
+++ src/com/osreboot/ridhvl/loader/HvlSequentialLoader.java
@@ -1,31 +1,36 @@
 package com.osreboot.ridhvl.loader;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class HvlSequentialLoader <T extends HvlContentLoader<?>>{
+public class HvlSequentialLoader{
 
-	private T loader;
 	private int progress;
-	private ArrayList<String> objects;
-	
-	public HvlSequentialLoader(T loaderArg, ArrayList<String> objectsArg){
-		loader = loaderArg;
+	private HashMap<String, ? extends HvlContentLoader<?>> objects;
+
+	public HvlSequentialLoader(HashMap<String, ? extends HvlContentLoader<?>> objectsArg){
 		objects = objectsArg;
 		progress = 0;
 	}
 
-	//TODO update and draw
-	
-	public T getLoader(){
-		return loader;
+	public void loadObject(){
+		if(progress < objects.size()){
+			String resource = new ArrayList<String>(objects.keySet()).get(progress);
+			objects.get(resource).loadResource(resource);
+		}
+		progress++;
 	}
 
-	public int getProgress(){
-		return progress;
+	public boolean isFinished(){
+		return progress == objects.size();
 	}
 
-	public ArrayList<String> getObjects(){
+	public float getProgress(){
+		return (float)progress/objects.size();
+	}
+
+	public HashMap<String, ? extends HvlContentLoader<?>> getObjects(){
 		return objects;
 	}
-	
+
 }
