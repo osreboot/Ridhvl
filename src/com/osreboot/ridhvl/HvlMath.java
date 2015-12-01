bac76c81a75228c516b2a100643197da123b3ddf
osreboot <calvin.16.weaver@gmail.com>
2015-11-30 22:10:48 -0800

    Close #147

--- src/com/osreboot/ridhvl/HvlMath.java
+++ src/com/osreboot/ridhvl/HvlMath.java
@@ -127,6 +127,10 @@ public class HvlMath {
 	public static float map(float x, float in_min, float in_max, float out_min, float out_max){
 		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 	}
+	
+	public static float constrain(float x, float min, float max){
+		return Math.min(Math.max(x, min), max);
+	}
 
 	public static class Stepper{
 
