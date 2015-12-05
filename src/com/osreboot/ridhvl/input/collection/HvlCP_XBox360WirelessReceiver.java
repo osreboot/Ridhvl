b7b27b56dc0bdf9d556faf0e49f5db88fe910678
osreboot <calvin.16.weaver@gmail.com>
2015-12-05 12:09:36 -0800

    Added HvlCP_XBox360WirelessReceiver and organized code

--- src/com/osreboot/ridhvl/input/collection/HvlCP_XBox360WirelessReceiver.java
+++ src/com/osreboot/ridhvl/input/collection/HvlCP_XBox360WirelessReceiver.java
@@ -1,27 +1,27 @@
 package com.osreboot.ridhvl.input.collection;
 
+import net.java.games.input.Controller;
+
 import com.osreboot.ridhvl.action.HvlAction2r;
 import com.osreboot.ridhvl.input.HvlControllerProfile;
 import com.osreboot.ridhvl.input.HvlPollValue;
 
-import net.java.games.input.Controller;
+public class HvlCP_XBox360WirelessReceiver extends HvlControllerProfile{
 
-public class HvlCP_LogitechF510 extends HvlControllerProfile{
-
-	public HvlCP_LogitechF510(){
-		super(HvlCP_LogitechF510.class, "Rumble Gamepad F510 (Controller)");
+	public HvlCP_XBox360WirelessReceiver(){
+		super(HvlCP_XBox360WirelessReceiver.class, "Controller (Xbox 360 Wireless Receiver for Windows)");
 	}
 	
-	@HvlPollValue(component = "X Axis", deadZone = 0.05f)
+	@HvlPollValue(component = "X Axis", deadZone = 0.2f)
 	public static final String JOY1X = "JOY1X";
 	
-	@HvlPollValue(component = "Y Axis", deadZone = 0.05f, amplifier = -1f)
+	@HvlPollValue(component = "Y Axis", deadZone = 0.2f, amplifier = -1f)
 	public static final String JOY1Y = "JOY1Y";
 	
-	@HvlPollValue(component = "X Rotation", deadZone = 0.05f)
+	@HvlPollValue(component = "X Rotation", deadZone = 0.2f)
 	public static final String JOY2X = "JOY2X";
 	
-	@HvlPollValue(component = "Y Rotation", deadZone = 0.05f, amplifier = -1f)
+	@HvlPollValue(component = "Y Rotation", deadZone = 0.2f, amplifier = -1f)
 	public static final String JOY2Y = "JOY2Y";
 	
 	@HvlPollValue(component = "Button 8")
@@ -104,4 +104,3 @@ public class HvlCP_LogitechF510 extends HvlControllerProfile{
 	};
 	
 }
-
--- src/com/osreboot/ridhvl/test/ExpandingRectangleTest.java
+++ src/com/osreboot/ridhvl/test/ExpandingRectangleTest.java
@@ -1,6 +1,6 @@
 package com.osreboot.ridhvl.test;
 
-import static com.osreboot.ridhvl.painter.painter2d.HvlPainter2D.getWhite512;
+import static com.osreboot.ridhvl.painter.painter2d.HvlPainter2D.*;
 
 import java.security.AccessController;
 import java.security.PrivilegedAction;
@@ -9,14 +9,14 @@ import org.newdawn.slick.Color;
 
 import com.osreboot.ridhvl.display.collection.HvlDisplayModeDefault;
 import com.osreboot.ridhvl.input.HvlControllerProfile;
-import com.osreboot.ridhvl.input.collection.HvlCP_LogitechF510;
+import com.osreboot.ridhvl.input.collection.HvlCPG_Gamepad;
 import com.osreboot.ridhvl.loader.HvlTextureLoader;
 import com.osreboot.ridhvl.painter.painter2d.HvlPainter2D;
 import com.osreboot.ridhvl.painter.painter2d.HvlTiledRect;
 import com.osreboot.ridhvl.template.HvlTemplate2D;
 
 public class ExpandingRectangleTest extends HvlTemplate2D {
-
+	
 	static {
 		HvlControllerProfile.setDebugOutput(true);
 
@@ -32,10 +32,6 @@ public class ExpandingRectangleTest extends HvlTemplate2D {
 		});
 	}
 	
-//	public static void main(String[] args) {
-//		new ExpandingRectangleTest();
-//	}
-	
 	public ExpandingRectangleTest() {
 		super(60, 1280, 720, "Ridhvl Expanding Rectangle Test", new HvlDisplayModeDefault());
 	}
@@ -45,11 +41,11 @@ public class ExpandingRectangleTest extends HvlTemplate2D {
 	
 	Color color;
 
-	HvlCP_LogitechF510 profile;
+	HvlCPG_Gamepad profile;
 	
 	@Override
 	public void initialize() {
-		profile = new HvlCP_LogitechF510();
+		profile = new HvlCPG_Gamepad();
 		textureLoader.loadResource("Icon");
 		color = Color.white;
 		testRect = new HvlTiledRect(textureLoader.getResource(0), 0.125f, 0.875f, 0.125f, 0.875f, 0, 0, 512, 256, 16, 16);
@@ -59,8 +55,8 @@ public class ExpandingRectangleTest extends HvlTemplate2D {
 	public void update(float delta) {
 		HvlPainter2D.hvlDrawQuad(0, 0, 1280, 720, getWhite512(), color);
 
-		testRect.setTotalWidth(testRect.getTotalWidth() + profile.getValue(HvlCP_LogitechF510.TRIGGER_LEFT) * 256 * delta);
-		testRect.setTotalHeight(testRect.getTotalHeight() + profile.getValue(HvlCP_LogitechF510.TRIGGER_RIGHT) * 256 * delta);
+		testRect.setTotalWidth(testRect.getTotalWidth() + profile.getValue(HvlCPG_Gamepad.TRIGGER_LEFT) * 256 * delta);
+		testRect.setTotalHeight(testRect.getTotalHeight() + profile.getValue(HvlCPG_Gamepad.TRIGGER_RIGHT) * 256 * delta);
 
 		testRect.setX(640 - (testRect.getTotalWidth()/2));
 		testRect.setY(360 - (testRect.getTotalHeight()/2));
