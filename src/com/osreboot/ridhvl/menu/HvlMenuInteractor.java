4590dd56d4e07ea998ccb1aa60d2a1638529459a
osreboot <calvin.16.weaver@gmail.com>
2015-12-23 12:33:50 -0800

    Added enable and disable events to HvlMenuInteractor

--- src/com/osreboot/ridhvl/menu/HvlMenuInteractor.java
+++ src/com/osreboot/ridhvl/menu/HvlMenuInteractor.java
@@ -3,6 +3,7 @@ package com.osreboot.ridhvl.menu;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.osreboot.ridhvl.action.HvlAction0;
 import com.osreboot.ridhvl.action.HvlAction1;
 import com.osreboot.ridhvl.input.HvlInput;
 import com.osreboot.ridhvl.menu.component.HvlButton;
@@ -24,14 +25,25 @@ public class HvlMenuInteractor {
 		}
 	};
 	
+	private static ArrayList<HvlAction0> enableEvents = new ArrayList<>(), disableEvents = new ArrayList<>();
+	
 	private static boolean enabled = false, clicking = false;
 	private static HvlComponent selected;
 	private static HvlInput up, down, select, toggle;
 	private static HashMap<HvlMenu, ArrayList<HvlComponent>> tabOrder = new HashMap<>();
 	private static int x = -1, y = -1;
 	
+	public static void addEnableEvent(HvlAction0 arg){
+		enableEvents.add(arg);
+	}
+	
+	public static void addDisableEvent(HvlAction0 arg){
+		disableEvents.add(arg);
+	}
+	
 	private static void update(float delta){
 		if(HvlCursor.getCursorX() != x || HvlCursor.getCursorY() != y){
+			if(enabled) for(HvlAction0 a : disableEvents) a.run();
 			enabled = false;
 			x = HvlCursor.getCursorX();
 			y = HvlCursor.getCursorY();
@@ -81,6 +93,7 @@ public class HvlMenuInteractor {
 				if(!enabled){
 					enabled = true;
 					if(getCurrentComponents().size() > 0) selected = (tabOrder.containsKey(HvlMenu.getCurrent()) ? tabOrder.get(HvlMenu.getCurrent()) : getCurrentComponents()).get(0);
+					for(HvlAction0 a : enableEvents) a.run();
 				}
 			}
 		});
