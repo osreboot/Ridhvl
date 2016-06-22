a9c0b795cb013e5ffba58c112276daa905b6b5bb
Adam Krpan <adam.krpan@outlook.com>
2016-06-22 15:11:53 -0700

    Added the HvlTextureUtil method "layerTextures", which will essentially merge two or more textures together like in an image editing program. Warning: this takes absolutely FOREVER: DO NOT CALL THIS MID-GAME.

--- src/com/osreboot/ridhvl/HvlTextureUtil.java
+++ src/com/osreboot/ridhvl/HvlTextureUtil.java
@@ -9,11 +9,52 @@ import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.util.BufferedImageUtil;
 
 public class HvlTextureUtil {
+	public static Texture layerTextures(Texture... layers) {
+		if (layers.length == 0) return null;
+		
+		int w = 0, h = 0;
+		
+		for (Texture t : layers) {
+			w = Math.max(w, t.getImageWidth());
+			h = Math.max(h, t.getImageHeight());
+		}
+		
+		BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		
+		for (int x = 0; x < w; x++) {
+			for (int y = 0; y < h; y++) {
+				float pixR, pixG, pixB, pixA;
+				
+				pixR = getPixelR(layers[0], x, y);
+				pixG = getPixelG(layers[0], x, y);
+				pixB = getPixelB(layers[0], x, y);
+				pixA = getPixelA(layers[0], x, y);
+				
+				for (int i = 1; i < layers.length; i++) {
+					if (x >= layers[i].getImageWidth() || y >= layers[i].getImageHeight()) continue;
+					
+					float srcA = pixA;
+					float destA = getPixelA(layers[i], x, y);
+					pixA = pixA + ((1f - pixA) * destA);
+					pixR = ((pixR * srcA) + (getPixelR(layers[i], x, y) * destA * (1f - srcA))) / pixA;
+					pixG = ((pixG * srcA) + (getPixelG(layers[i], x, y) * destA * (1f - srcA))) / pixA;
+					pixB = ((pixB * srcA) + (getPixelB(layers[i], x, y) * destA * (1f - srcA))) / pixA;
+				}
+				
+				img.setRGB(x, y, ((int)(pixA * 255) << 24) | ((int)(pixR * 255) << 16) | ((int)(pixG * 255) << 8) | (int)(pixB * 255));
+			}
+		}
+		
+		try {
+			return BufferedImageUtil.getTexture("layered", img);
+		} catch (IOException e) {
+			return null;
+		}
+	}
+	
 	public static Texture getColoredRect(int width, int height, Color color)
 	{
 		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		
-//		System.out.println(color.getRed());
 		
 		java.awt.Color awtColor = new java.awt.Color(color.r, color.g, color.b, color.a);
 		
