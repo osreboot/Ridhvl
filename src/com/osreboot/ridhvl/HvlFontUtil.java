c36779f12e92bc1d39df3aa385193eff9144f546
osreboot <calvin.16.weaver@gmail.com>
2017-08-07 20:42:39 -0700

    Added FLAT char set

--- src/com/osreboot/ridhvl/HvlFontUtil.java
+++ src/com/osreboot/ridhvl/HvlFontUtil.java
@@ -2,14 +2,16 @@ package com.osreboot.ridhvl;
 
 public class HvlFontUtil {
 
-	public static final char[] DEFAULT				 = "abcdefghijklmnopqrstuvwxyz0123456789!?.()[]".toCharArray();
-	public static final char[] DEFAULTCASE			 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?.()[]".toCharArray();
-	public static final char[] NUMERICAL			 = "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~".toCharArray();
-	public static final char[] SIMPLISTIC			 = "abcdefghijklmnopqrstuvwxyz1234567890.!?\"()[]:;-+".toCharArray();
-	public static final char[] SIMPLISTICCASE		 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.!?\"()[]:;-+".toCharArray();
-	public static final char[] MATHEMATICS			 = "1234567890+-*/".toCharArray();
-	public static final char[] COMPUTATIONAL		 = "abcdefghijklmnopqrstuvwxyz0123456789/\\'\")(][?!.,_@*^-+=|:;><".toCharArray();
-	public static final char[] COMPUTATIONALCASE	 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/\\'\")(][?!.,_@*^-+=|;:><abcdefghijklmnopqrstuvwxyz".toCharArray();
+	public static final char[] DEFAULT				= "abcdefghijklmnopqrstuvwxyz0123456789!?.()[]".toCharArray();
+	public static final char[] DEFAULTCASE			= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?.()[]".toCharArray();
+	public static final char[] NUMERICAL			= "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~".toCharArray();
+	public static final char[] SIMPLISTIC			= "abcdefghijklmnopqrstuvwxyz1234567890.!?\"()[]:;-+".toCharArray();
+	public static final char[] SIMPLISTICCASE		= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.!?\"()[]:;-+".toCharArray();
+	public static final char[] MATHEMATICS			= "1234567890+-*/".toCharArray();
+	public static final char[] COMPUTATIONAL		= "abcdefghijklmnopqrstuvwxyz0123456789/\\'\")(][?!.,_@*^-+=|:;><".toCharArray();
+	public static final char[] COMPUTATIONALCASE	= "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/\\'\")(][?!.,_@*^-+=|;:><abcdefghijklmnopqrstuvwxyz".toCharArray();
+	public static final char[] FLAT					= "abcdefghijklmnopqrstuvwxyz0123456789_".toCharArray();
+	public static final char[] FLATCASE				= "abcdefghijklmnopqrstuvwxyz0123456789_ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 	
 	public static boolean containsChar(char[] layout, char c){
 		for(int i = 0; i < layout.length; i++){
