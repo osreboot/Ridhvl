0920a133c30cbc4e0eb8c1203e76ec7d1597d66f
osreboot <calvin.16.weaver@gmail.com>
2017-08-22 12:42:47 -0700

    Fixed a stupid because I'm colorblind

--- src/com/osreboot/ridhvl/HvlColor.java
+++ src/com/osreboot/ridhvl/HvlColor.java
@@ -4,6 +4,6 @@ import org.newdawn.slick.Color;
 
 public class HvlColor {
 
-	public static final Color HYPRGLOO_ORANGE = new Color(255, 64, 0);
+	public static final Color HYPRGLOO_RED = new Color(255, 64, 0);
 	
 }
